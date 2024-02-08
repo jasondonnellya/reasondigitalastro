be370ebd6770e49bd643bd47b2413fcc872aa99b
Jason Donnelly <Jason.jimmy4@gmail.com>
2024-02-08 16:58:22 +0000

    Trying to run as hybrid site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import cloudflare from "@astrojs/cloudflare";
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'static',
+  output: 'hybrid',
   integrations: [vue({
     appEntrypoint: '/src/main',
     // https://github.com/withastro/astro/issues/9328
@@ -13,5 +14,12 @@ export default defineConfig({
         includeAbsolute: false,
       },
     }
-  })]
+  })],
+  adapter: cloudflare({
+    mode: 'directory',
+    routes: {
+      strategy: 'include',
+      include: ['/index.html'],
+    },
+  })
 });
